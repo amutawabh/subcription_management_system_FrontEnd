ef069427cc1876eb3276d8e8d13dafc1d023cfe4
useradmin <amutawa@hotmail.com>
2024-12-11 23:28:42 +0300

    update

--- src/services/subscriptionService.js
+++ src/services/subscriptionService.js
@@ -3,43 +3,43 @@
 import api from './api';
 
 export const getSubscriptions = async () => {
-    const token = localStorage.getItem('token'); // استرجاع التوكن من التخزين المحلي
+    const token = localStorage.getItem('token'); 
     const response = await api.get('/subscriptions', {
         headers: {
-            Authorization: `Bearer ${token}`, // إضافة التوكن إلى الهيدر
+            Authorization: `Bearer ${token}`, 
         },
     });
-    return response.data; // إرجاع البيانات المستلمة
+    return response.data; 
 };
 
 export const addSubscription = async (subscriptionData) => {
-    const token = localStorage.getItem('token'); // استرجاع التوكن من التخزين المحلي
+    const token = localStorage.getItem('token'); 
     const response = await api.post('/subscriptions', subscriptionData, {
         headers: {
-            Authorization: `Bearer ${token}`, // إضافة التوكن إلى الهيدر
+            Authorization: `Bearer ${token}`, 
         },
     });
-    return response.data; // إرجاع البيانات المستلمة
+    return response.data; 
 };
 
-// دالة تحديث الاشتراك
+
 export const updateSubscription = async (id, subscriptionData) => {
-    const token = localStorage.getItem('token'); // استرجاع التوكن من التخزين المحلي
+    const token = localStorage.getItem('token'); 
     const response = await api.put(`/subscriptions/${id}`, subscriptionData, {
         headers: {
-            Authorization: `Bearer ${token}`, // إضافة التوكن إلى الهيدر
+            Authorization: `Bearer ${token}`, 
         },
     });
-    return response.data; // إرجاع البيانات المستلمة
+    return response.data; 
 };
 
-// دالة حذف الاشتراك
+
 export const deleteSubscription = async (id) => {
-    const token = localStorage.getItem('token'); // استرجاع التوكن من التخزين المحلي
+    const token = localStorage.getItem('token'); 
     const response = await api.delete(`/subscriptions/${id}`, {
         headers: {
-            Authorization: `Bearer ${token}`, // إضافة التوكن إلى الهيدر
+            Authorization: `Bearer ${token}`, 
         },
     });
-    return response.data; // إرجاع البيانات المستلمة
+    return response.data; 
 };
